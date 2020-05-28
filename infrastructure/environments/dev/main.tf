5522f5a660250c65daa37f8aa47d5af1bec86176
Matt Paul <mattpaul001@icloud.com>
2020-05-28 12:18:38 +0100

    Terraform the cluster

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -15,7 +15,6 @@ terraform {
   }
 }
 
-
 resource "azurerm_resource_group" "platform" {
   name     = "platform-${var.USERNAME}"
   location = var.REGION
@@ -25,18 +24,87 @@ resource "azurerm_resource_group" "platform" {
   }
 }
 
-# Create virtual network
 resource "azurerm_virtual_network" "platform" {
   name                = "platform-${var.USERNAME}"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.0.0.0/8"]
   location            = var.REGION
   resource_group_name = azurerm_resource_group.platform.name
+  tags = {
+    environment = "dev-${var.USERNAME}"
+  }
 }
 
-# Create subnet
-resource "azurerm_subnet" "cluster-nodes" {
+resource "azurerm_subnet" "cluster_nodes" {
   name                 = "cluster-nodes-${var.USERNAME}"
   resource_group_name  = azurerm_resource_group.platform.name
   virtual_network_name = azurerm_virtual_network.platform.name
-  address_prefixes     = ["10.0.0.0/26"]
+  address_prefixes     = ["10.240.0.0/16"]
+}
+
+resource "azurerm_kubernetes_cluster" "cluster" {
+  name                = var.USERNAME
+  location            = var.REGION
+  dns_prefix          = var.USERNAME
+  resource_group_name = azurerm_resource_group.platform.name
+
+  default_node_pool {
+    name                = "default"
+    enable_auto_scaling = true
+    max_count           = 2
+    min_count           = 1
+    vm_size             = "Standard_D2_v2"
+    vnet_subnet_id      = azurerm_subnet.cluster_nodes.id
+    availability_zones  = ["1", "2", "3"]
+    tags = {
+      environment = "dev-${var.USERNAME}"
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  network_profile {
+    network_plugin = "kubenet"
+  }
+
+  addon_profile {
+    aci_connector_linux {
+      enabled = false
+    }
+    azure_policy {
+      enabled = false
+    }
+    http_application_routing {
+      enabled = false
+    }
+    kube_dashboard {
+      enabled = false
+    }
+    oms_agent {
+      enabled                    = true
+      log_analytics_workspace_id = azurerm_log_analytics_workspace.cluster.id
+    }
+  }
+
+  tags = {
+    environment = "dev-${var.USERNAME}"
+  }
+}
+
+
+resource "azurerm_log_analytics_workspace" "cluster" {
+  name                = "cluster-${var.USERNAME}"
+  location            = var.REGION
+  resource_group_name = azurerm_resource_group.platform.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+
+  tags = {
+    environment = "dev-${var.USERNAME}"
+  }
 }
--- infrastructure/modules/cluster/main.tf
+++ infrastructure/modules/cluster/main.tf
@@ -8,10 +8,3 @@ resource "azurerm_public_ip" "dev_ip" {
     environment = var.environment
   }
 }
-
-resource "azurerm_subnet" "cluster" {
-  name                 = var.cluster_subnet_name
-  resource_group_name  = var.vnet_resource_group
-  address_prefixes     = ["10.0.0.0/26"]
-  virtual_network_name = var.vnet_name
-}
