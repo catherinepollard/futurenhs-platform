30ad2878ccbc9ee64bb0882107222bd1d5a991a8
Tracy Wu <sytracywu@gmail.com>
2020-05-28 11:10:36 +0100

    added subnet and vn

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -10,14 +10,14 @@ provider "random" {
 terraform {
   required_version = "0.12.25"
   backend "azurerm" {
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
+    container_name = "tfstate"
+    key            = "dev.terraform.tfstate"
   }
 }
 
 
 resource "azurerm_resource_group" "platform" {
-  name     = "rg-${var.USERNAME}"
+  name     = "platform-${var.USERNAME}"
   location = var.REGION
 
   tags = {
@@ -25,21 +25,18 @@ resource "azurerm_resource_group" "platform" {
   }
 }
 
-module cluster {
-  source = "../../modules/cluster"
+# Create virtual network
+resource "azurerm_virtual_network" "platform" {
+  name                = "platform-${var.USERNAME}"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.REGION
+  resource_group_name = azurerm_resource_group.platform.name
+}
 
-  # client_id                             = var.CLIENT_ID
-  # client_secret                         = var.CLIENT_SECRET
-  environment                           = "dev-${var.USERNAME}"
-  location                              = var.REGION
-  resource_group_name                   = azurerm_resource_group.platform.name
-  # vnet_name                             = azurerm_virtual_network.cluster.name
-  # vnet_resource_group                   = azurerm_virtual_network.cluster.resource_group_name
-  # lb_subnet_id                          = azurerm_subnet.load_balancer.id
-  # cluster_subnet_name                   = "adarz-spoke-products-sn-02"
-  # cluster_subnet_cidr                   = "10.5.65.192/26"
-  # cluster_route_destination_cidr_blocks = var.CLUSTER_ROUTE_DESTINATION_CIDR_BLOCKS
-  # cluster_route_next_hop                = var.CLUSTER_ROUTE_NEXT_HOP
-  # lb_route_table_id                     = azurerm_route_table.load_balancer.id
-  # support_email_addresses               = var.SUPPORT_EMAIL_ADDRESSES
+# Create subnet
+resource "azurerm_subnet" "cluster-nodes" {
+  name                 = "cluster-nodes-${var.USERNAME}"
+  resource_group_name  = azurerm_resource_group.platform.name
+  virtual_network_name = azurerm_virtual_network.platform.name
+  address_prefixes     = ["10.0.0.0/26"]
 }
