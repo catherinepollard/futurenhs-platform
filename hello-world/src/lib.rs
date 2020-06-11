fb1d3ef5ccabe5bbec93459358aa4fb4f7ca428c
Matt Paul <mattpaul001@icloud.com>
2020-06-11 16:18:41 +0100

    Add tracing to hello world app

--- hello-world/src/lib.rs
+++ hello-world/src/lib.rs
@@ -1,17 +1,25 @@
 use tide::{Request, Server};
+use tracing::{event, Level};
 
+#[tracing::instrument]
 pub fn create_app() -> Server<()> {
     let mut app = tide::new();
     app.at("/hello/:name").get(hello_handler);
     app
 }
 
+#[tracing::instrument]
 async fn hello_handler(req: Request<()>) -> tide::Result<String> {
     let name = req.param("name")?;
+    event!(Level::INFO, where_am_i = "inside handler!");
+
     Ok(hello(name))
 }
 
+#[tracing::instrument]
 fn hello(name: String) -> String {
+    event!(Level::INFO, where_am_i = "inside hello function!");
+
     format!("Hello, {}\n", name)
 }
 
@@ -21,6 +29,6 @@ mod test {
 
     #[test]
     fn test_hello() {
-      assert_eq!("Hello, SomeName\n", hello(String::from("SomeName")));
+        assert_eq!("Hello, SomeName\n", hello(String::from("SomeName")));
     }
 }
--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,3 +1,14 @@
+use tracing::Level;
+
+#[tracing::instrument]
 fn main() -> std::io::Result<()> {
+    let subscriber = tracing_subscriber::fmt::Subscriber::builder()
+        .json()
+        .with_timer(tracing_subscriber::fmt::time::ChronoUtc::rfc3339())
+        .with_max_level(Level::DEBUG)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
     smol::run(hello_world::create_app().listen("0.0.0.0:3030"))
 }
