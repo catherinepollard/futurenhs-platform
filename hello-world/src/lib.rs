e00b056610f4254c67ca2c928e2f0e1fc3946fd8
Matt Paul <mattpaul001@icloud.com>
2020-06-16 14:32:16 +0100

    Use appinsights crate

--- hello-world/src/lib.rs
+++ hello-world/src/lib.rs
@@ -1,13 +1,36 @@
+use appinsights::telemetry::{RequestTelemetry, SeverityLevel};
+use appinsights::{InMemoryChannel, TelemetryClient};
+use http::Method;
 use tide::{Request, Server};
 
-pub fn create_app() -> Server<()> {
-    let mut app = tide::new();
+pub struct State {
+    client: TelemetryClient<InMemoryChannel>,
+}
+
+pub fn create_app(client: TelemetryClient<InMemoryChannel>) -> Server<State> {
+    client.track_event("*** event inside create_app");
+    client.track_trace(
+        "*** trace within create_app function",
+        SeverityLevel::Warning,
+    );
+
+    let state = State { client };
+
+    let mut app = tide::with_state(state);
+
     app.at("/hello/:name").get(hello_handler);
     app
 }
 
-async fn hello_handler(req: Request<()>) -> tide::Result<String> {
+async fn hello_handler(req: Request<State>) -> tide::Result<String> {
+    let telemetry = RequestTelemetry::new(
+        Method::GET,
+        "https://example.com/main.html".parse().unwrap(),
+        std::time::Duration::from_secs(2),
+        "200",
+    );
     let name = req.param("name")?;
+    req.state().client.track(telemetry);
     Ok(hello(name))
 }
 
@@ -21,6 +44,6 @@ mod test {
 
     #[test]
     fn test_hello() {
-      assert_eq!("Hello, SomeName\n", hello(String::from("SomeName")));
+        assert_eq!("Hello, SomeName\n", hello(String::from("SomeName")));
     }
 }
--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,3 +1,10 @@
+use appinsights::telemetry::SeverityLevel;
+use appinsights::TelemetryClient;
 fn main() -> std::io::Result<()> {
-    smol::run(hello_world::create_app().listen("0.0.0.0:3030"))
+    let id = "0b902312-dbaa-4ac0-bd21-029752baeb37";
+    let client = TelemetryClient::new(id.to_string());
+    client.track_event("*** inside main function");
+    client.track_trace("*** within main function", SeverityLevel::Warning);
+    let result = smol::run(hello_world::create_app(client).listen("0.0.0.0:3030"));
+    result
 }
