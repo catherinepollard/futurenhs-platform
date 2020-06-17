d3f055bdec9c4f77e89443e4e792ca8c44e9e439
Matt Paul <mattpaul001@icloud.com>
2020-06-17 15:15:59 +0100

    Trying parent_id

--- hello-world/src/lib.rs
+++ hello-world/src/lib.rs
@@ -1,3 +1,4 @@
+use appinsights::telemetry::Telemetry;
 use appinsights::telemetry::{RequestTelemetry, SeverityLevel};
 use appinsights::{InMemoryChannel, TelemetryClient};
 use http::Method;
@@ -5,16 +6,26 @@ use tide::{Request, Server};
 
 pub struct State {
     client: TelemetryClient<InMemoryChannel>,
+    parent_id: String,
 }
 
 pub fn create_app(client: TelemetryClient<InMemoryChannel>) -> Server<State> {
-    client.track_event("*** event inside create_app");
+    client.track_event("Track event - inside create_app");
     client.track_trace(
         "*** trace within create_app function",
         SeverityLevel::Warning,
     );
 
-    let state = State { client };
+    let telemetry = RequestTelemetry::new(
+        Method::GET,
+        "https://example.com/main.html".parse().unwrap(),
+        std::time::Duration::from_secs(2),
+        "200",
+    );
+
+    client.track(telemetry);
+
+    let state = State { client, parent_id: "1".to_string() };
 
     let mut app = tide::with_state(state);
 
@@ -23,14 +34,23 @@ pub fn create_app(client: TelemetryClient<InMemoryChannel>) -> Server<State> {
 }
 
 async fn hello_handler(req: Request<State>) -> tide::Result<String> {
-    let telemetry = RequestTelemetry::new(
+    let mut telemetry = RequestTelemetry::new(
         Method::GET,
-        "https://example.com/main.html".parse().unwrap(),
+        "https://example.com/hello/main.html".parse().unwrap(),
         std::time::Duration::from_secs(2),
         "200",
     );
+
+    let parent_id = req.state().parent_id.clone();
+
+    telemetry
+        .tags_mut()
+        .operation_mut()
+        .set_parent_id(parent_id);
+
     let name = req.param("name")?;
     req.state().client.track(telemetry);
+
     Ok(hello(name))
 }
 
--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,10 +1,8 @@
-use appinsights::telemetry::SeverityLevel;
 use appinsights::TelemetryClient;
 fn main() -> std::io::Result<()> {
-    let id = "0b902312-dbaa-4ac0-bd21-029752baeb37";
-    let client = TelemetryClient::new(id.to_string());
-    client.track_event("*** inside main function");
-    client.track_trace("*** within main function", SeverityLevel::Warning);
+    let client = TelemetryClient::new("0b902312-dbaa-4ac0-bd21-029752baeb37".to_string());
+
+    client.track_event("Track event - main function");
     let result = smol::run(hello_world::create_app(client).listen("0.0.0.0:3030"));
     result
 }
