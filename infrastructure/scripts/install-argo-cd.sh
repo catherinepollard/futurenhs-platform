6f72f5c95d9462f3ce22190f365f5be607d11170
Tracy Wu <sytracywu@gmail.com>
2020-06-18 15:22:18 +0100

    install argo cd cli

--- infrastructure/scripts/install-argo-cd.sh
+++ infrastructure/scripts/install-argo-cd.sh
@@ -11,7 +11,11 @@ while true; do
             kubectl create namespace argocd || { echo 'Create namespace argocd failed. Please fix issues before proceeding. Perhaps you have already created an argocd namespace?' ; exit 1; }
             echo "Installing Argo CD"
             kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml || { echo 'Unable to install.' ; exit 1; }
-            echo Pods will need to be in a running state before you can move forward. These need to be in a running state before you can move forward. Use 'watch kubectl get pods -n argocd' to check"
+            echo "Pods will need to be in a running state before you can move forward. These need to be in a running state before you can move forward. Use 'watch kubectl get pods -n argocd' to check"
+            echo "Installing Argo CD CLI"
+            brew tap argoproj/tap || { echo 'Unable to install. Do you have brew installed?' ; exit 1;  }
+            echo " brew install Argo CD"
+            brew install argoproj/tap/argocd brew tap argoproj/tap || { echo 'Unable to install.' ; exit 1;  }
 
         break;;
         [Nn]* ) echo "You may wish to use the commands 'az account set' and 'az aks get-credentials' to change cluster."; exit;;
