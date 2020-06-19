1407d05f20c6d24cc493fad92c496674957e2c09
Matt Paul <mattpaul001@icloud.com>
2020-06-19 09:00:31 +0100

    Remove unecceasary let

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -3,6 +3,5 @@ fn main() -> std::io::Result<()> {
     let client = TelemetryClient::new("0b902312-dbaa-4ac0-bd21-029752baeb37".to_string());
 
     client.track_event("Track event - main function");
-    let result = smol::run(hello_world::create_app(client).listen("0.0.0.0:3030"));
-    result
+    smol::run(hello_world::create_app(client).listen("0.0.0.0:3030"))
 }
