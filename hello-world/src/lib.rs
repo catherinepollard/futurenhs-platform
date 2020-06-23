2a4dacef751d71f4dfb63d717df1bb7336610901
Matt Paul <mattpaul001@icloud.com>
2020-06-23 11:35:54 +0100

    Use opentelemetry-application-insights crate

--- hello-world/src/lib.rs
+++ hello-world/src/lib.rs
@@ -1,25 +1,49 @@
-use tide::{Request, Server};
-use tracing::{event, Level};
+use std::future::Future;
+use std::pin::Pin;
+use tide::{Next, Request, Result, Server};
+use tracing::{info, info_span};
+use tracing_futures::Instrument;
 
-#[tracing::instrument]
-pub fn create_app() -> Server<()> {
-    let mut app = tide::new();
+#[derive(Debug)]
+pub struct State {}
+
+fn log<'a>(
+    req: Request<State>,
+    next: Next<'a, State>,
+) -> Pin<Box<dyn Future<Output = Result> + Send + 'a>> {
+    Box::pin(async {
+        let span = info_span!(
+            "request",
+            http.status_code = tracing::field::Empty,
+            http.method = req.method().as_ref(),
+            http.target = req.url().as_ref()
+        );
+
+        let res = next.run(req).instrument(span.clone()).await?;
+        span.record("http.status_code", &u16::from(res.status()));
+
+        Ok(res)
+    })
+}
+
+pub fn create_app() -> Server<State> {
+    let mut app = tide::with_state(State {});
+    app.middleware(log);
+
     app.at("/hello/:name").get(hello_handler);
     app
 }
 
 #[tracing::instrument]
-async fn hello_handler(req: Request<()>) -> tide::Result<String> {
+async fn hello_handler(req: Request<State>) -> tide::Result<String> {
     let name = req.param("name")?;
-    event!(Level::INFO, where_am_i = "inside handler!");
+    info!("The users name is: {}", name);
 
     Ok(hello(name))
 }
 
 #[tracing::instrument]
 fn hello(name: String) -> String {
-    event!(Level::INFO, where_am_i = "inside hello function!");
-
     format!("Hello, {}\n", name)
 }
 
--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,14 +1,23 @@
-use tracing::Level;
+use opentelemetry::{api::Provider, sdk, sdk::BatchSpanProcessor};
+use std::env;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::Registry;
 
-#[tracing::instrument]
-fn main() -> std::io::Result<()> {
-    let subscriber = tracing_subscriber::fmt::Subscriber::builder()
-        .json()
-        .with_timer(tracing_subscriber::fmt::time::ChronoUtc::rfc3339())
-        .with_max_level(Level::DEBUG)
-        .finish();
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let instrumentation_key =
+        env::var("INSTRUMENTATION_KEY").expect("env var INSTRUMENTATION_KEY should exist");
+    let exporter = opentelemetry_application_insights::Exporter::new(instrumentation_key);
+    let batch_exporter =
+        BatchSpanProcessor::builder(exporter, tokio::spawn, tokio::time::interval).build();
+    let provider = sdk::Provider::builder()
+        .with_batch_exporter(batch_exporter)
+        .build();
 
-    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+    let tracer = provider.get_tracer("example-tracing");
+    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+    let subscriber = Registry::default().with(telemetry);
+    tracing::subscriber::set_global_default(subscriber).expect("setting global default failed");
 
-    smol::run(hello_world::create_app().listen("0.0.0.0:3030"))
+    hello_world::create_app().listen("0.0.0.0:3030").await
 }
