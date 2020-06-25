aeade6351b206c08e599cc019983953d0e8b2145
declanslevin <33623842+declanslevin@users.noreply.github.com>
2020-06-25 15:34:54 +0100

    remove cypress dir

--- cypress/cypress/integration/hello_world/hello_world_rust.js
+++
@@ -1,8 +0,0 @@
-describe("Hello World", () => {
-  it("Makes request", () => {
-    cy.request("http://127.0.0.1:3030/hello/Cypress").then((res) => {
-      expect(res.status).to.equal(200);
-      expect(res.body).to.contain("Hello, Cypress");
-    });
-  });
-});
