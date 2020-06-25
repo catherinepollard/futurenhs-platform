061096fb235cff8eed351f8c850c1a7b511a072d
Jamie Potter <cobwebfx@gmail.com>
2020-06-25 11:03:28 +0100

    restructure directories again and move non env related patches into base

--- infrastructure/scripts/setup-ingress.sh
+++ infrastructure/scripts/setup-ingress.sh
@@ -7,16 +7,20 @@ EMAIL="${2:?"Please enter your email address as second argument"}"
 ENV="${3:-dev}"
 REGION="${4:-westeurope}"
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+DNS_PREFIX=$NAME
+if [ $ENV == "prod" ]; then
+  DNS_PREFIX="fnhs"
+fi
 
 echo " > Setup Hello World Service"
 # hello-world/dev used because currently no prod version
 kubectl apply -f "$CURRENT_DIR/../../hello-world/dev"
 
 echo " > Combine and execute Ingress Manifests (Step 1)"
-kustomize build "$CURRENT_DIR/../kubernetes/ingress/step1/overlays/$ENV" --reorder none |
+kustomize build "$CURRENT_DIR/../kubernetes/ingress/step1/$ENV" --reorder none |
   sed \
     -e "s/--USERNAME--/$NAME/g" \
-    -e "s/--DNS_PREFIX--/$NAME/g" \
+    -e "s/--DNS_PREFIX--/$DNS_PREFIX/g" \
     -e "s/--REGION--/$REGION/g" \
     -e "s/--EMAIL--/$EMAIL/g" |
   kubectl apply -f -
@@ -28,16 +32,16 @@ echo " > Waiting for ingress-nginx-controller deployment to be loaded"
 kubectl rollout status deployment ingress-nginx-controller -n ingress-nginx
 
 echo " > Combine and execute Ingress Manifests (Step 2)"
-kustomize build "$CURRENT_DIR/../kubernetes/ingress/step2/overlays/$ENV" --reorder none |
+kustomize build "$CURRENT_DIR/../kubernetes/ingress/step2/$ENV" --reorder none |
   sed \
     -e "s/--USERNAME--/$NAME/g" \
-    -e "s/--DNS_PREFIX--/$NAME/g" \
+    -e "s/--DNS_PREFIX--/$DNS_PREFIX/g" \
     -e "s/--REGION--/$REGION/g" \
     -e "s/--EMAIL--/$EMAIL/g" |
   kubectl apply -f -
 
 echo "After a minute or two, you will be able to browse to:"
-printf "\nhttps://$NAME.$REGION.cloudapp.azure.com/hello/$NAME\n\n"
+printf "\nhttps://$DNS_PREFIX.$REGION.cloudapp.azure.com/hello/$NAME\n\n"
 echo "The browser will moan that this certificate is fake."
 echo "That's because it's a staging certificate."
 echo "Click Advanced, and Proceed"
