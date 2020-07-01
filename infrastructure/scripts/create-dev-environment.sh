102fdbb8deb8e8f6aaada39195bc13e268757cb2
Jan Kuehle <jan.kuehle@red-badger.com>
2020-07-01 11:35:13 +0100

    Generate ingress overlay

--- infrastructure/scripts/create-dev-environment.sh
+++ infrastructure/scripts/create-dev-environment.sh
@@ -4,43 +4,64 @@ set -eu
 
 NAME="${1:?"Please enter your name as first argument"}"
 
-REPO_ROOT="$(git rev-parse --show-toplevel)"
-DEV_CONFIG_FILE="$REPO_ROOT/infrastructure/environments/dev/terraform.tfvars"
-
-SUBSCRIPTION_ID=4a4be66c-9000-4906-8253-6a73f09f418d
-RESOURCE_GROUP_NAME=tfstate$NAME
-STORAGE_ACCOUNT_NAME=fnhstfstatedev$NAME
-CONTAINER_NAME=tfstate
-
-if [ -f "$DEV_CONFIG_FILE" ]; then
-	echo "File infrastructure/environments/dev/terraform.tfvars already exists."
-	echo "If you want to initialize your environment again, please delete the file and rerun this script."
+if [ "$NAME" != "${NAME//[^a-z]/-}" ]; then
+	echo "Name can only contain lowercase characters a-z"
 	exit 1
 fi
 
-# Use non-production subscription
-az account set --subscription $SUBSCRIPTION_ID
+REPO_ROOT="$(git rev-parse --show-toplevel)"
 
-# Create resource group
-# TODO: do we want to limit this to england/wales, or is europe okay?
-az group create --name $RESOURCE_GROUP_NAME --location westeurope
+setup_terraform() {
+	DEV_CONFIG_FILE="$REPO_ROOT/infrastructure/environments/dev/terraform.tfvars"
 
-# Create storage account
-az storage account create --kind StorageV2 --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+	SUBSCRIPTION_ID=4a4be66c-9000-4906-8253-6a73f09f418d
+	RESOURCE_GROUP_NAME=tfstate$NAME
+	STORAGE_ACCOUNT_NAME=fnhstfstatedev$NAME
+	CONTAINER_NAME=tfstate
 
-# Get storage account key
-ACCESS_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query [0].value -o tsv)
+	if [ -f "$DEV_CONFIG_FILE" ]; then
+		echo "File infrastructure/environments/dev/terraform.tfvars already exists."
+		echo "If you want to initialize your environment again, please delete the file and rerun this script."
+		exit 1
+	fi
 
-# Create blob container
-az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --account-key "$ACCESS_KEY"
+	# Use non-production subscription
+	az account set --subscription $SUBSCRIPTION_ID
 
-cat >"$DEV_CONFIG_FILE" <<EOF
+	# Create resource group
+	# TODO: do we want to limit this to england/wales, or is europe okay?
+	az group create --name $RESOURCE_GROUP_NAME --location westeurope
+
+	# Create storage account
+	az storage account create --kind StorageV2 --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+
+	# Get storage account key
+	ACCESS_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query [0].value -o tsv)
+
+	# Create blob container
+	az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --account-key "$ACCESS_KEY"
+
+	cat >"$DEV_CONFIG_FILE" <<EOF
 resource_group_name="$RESOURCE_GROUP_NAME"
 storage_account_name="$STORAGE_ACCOUNT_NAME"
 USERNAME="$NAME"
 EOF
 
-echo "Your dev terraform environment is ready to go. To initialize run:"
-echo ""
-echo "    cd $REPO_ROOT/infrastructure/environments/dev"
-echo "    terraform init -backend-config=terraform.tfvars"
+	echo "Your dev terraform environment is ready to go. To initialize run:"
+	echo ""
+	echo "    cd $REPO_ROOT/infrastructure/environments/dev"
+	echo "    terraform init -backend-config=terraform.tfvars"
+}
+
+setup_ingress_overlay() {
+	INGRESS_DIR="$REPO_ROOT/infrastructure/kubernetes/ingress"
+	mkdir -p "$INGRESS_DIR/dev"
+
+	for file in $INGRESS_DIR/dev-template/*.yaml; do
+		filename="$(basename "$file")"
+		sed "s/{{NAME}}/$NAME/g" $file > "$INGRESS_DIR/dev/$filename"
+	done
+}
+
+setup_terraform
+setup_ingress_overlay
